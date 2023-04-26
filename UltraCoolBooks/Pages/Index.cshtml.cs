2be95cc4bb26a061109d1697ecbaa5d781127a10
znooker <znooker_18@hotmail.com>
2023-04-26 15:49:59 +0200

    Imported Main site
    Imported Main site

--- UltraCoolBooks/Pages/Index.cshtml.cs
+++ UltraCoolBooks/Pages/Index.cshtml.cs
@@ -1,20 +1,100 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UltraCoolBooks.Data;
+using UltraCoolBooks.Models;
 
 namespace UltraCoolBooks.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+
+
+    
+        private readonly UltraCoolBooks.Data.ApplicationDbContext _context;
+
+        public IndexModel(UltraCoolBooks.Data.ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
+        public IList<Models.Book> Books { get; set; } = default!;
+        public Book RandomBook { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        public async Task OnGetAsync()
         {
+            if (string.IsNullOrEmpty(SearchString))
+            {
+                if (_context.Books != null)
+                {
+                    Books = await _context.Books
+                         .Where(e => e.isDeleted == false)
+                    .Include(e => e.AuthorBooks)
+                    .ThenInclude(e => e.Author)
+                    .ToListAsync();
+
+                    var bookIds = await _context.Books
+                    .Where(e => e.isDeleted == false)
+                    .Select(e => e.BookId)
+                    .ToListAsync();
+
+                    var random = new Random();
+                    var bookIdsList = await _context.Books
+                                        .Where(e => e.isDeleted == false)
+                                        .Select(e => e.BookId)
+                                        .ToListAsync();
+
+                    var randomIndex = random.Next(bookIds.Count);
+                    //var randomBookId = bookIds[randomIndex];
+
+           
+              
+                    
+                  
+                    //    RandomBook = _context.Books
+                    //         .Where(e => e.BookId == randomBookId)
+                    //        .Include(e => e.AuthorBooks)
+                    //        .ThenInclude(e => e.Author)
+                    //         .Include(e => e.BookGenres)
+                    //        .ThenInclude(e => e.GenresGenre)
+                    //        .FirstOrDefault();
+
+                   
+                }
+            }
+            else 
+            {
+                Books = await _context.Books
+                     .Where(e => e.isDeleted == false)
+                   .Where(e => e.Title.Contains(SearchString) ||
+                            e.AuthorBooks.Any(e => e.Author.FirstName.Contains(SearchString)
+                            || e.Author.LastName.Contains(SearchString))
+                            ||e.BookGenres.Any(e => e.GenresGenre.Title.Contains(SearchString)))
+                  .Include(e => e.BookGenres)
+                  .ThenInclude(e => e.GenresGenre)
+                  .Include(e => e.AuthorBooks)
+                  .ThenInclude(e => e.Author)
+                  .ToListAsync();
+
+            
+            }
 
         }
+
+   
     }
+
+
+
+
 }
+
