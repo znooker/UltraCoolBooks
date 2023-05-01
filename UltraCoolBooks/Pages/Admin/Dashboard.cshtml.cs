2912be3c7bc6ca8d8600d2ed73e65c612fbde9b2
sala3132156 <116171488+sala3132156@users.noreply.github.com>
2023-05-01 17:44:59 +0200

    charts

---
+++ UltraCoolBooks/Pages/Admin/Dashboard.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Syncfusion.EJ2.Charts;
+using Microsoft.EntityFrameworkCore;
+using UltraCoolBooks.Models;
+
+namespace UltraCoolBooks.Pages.Admin
+{
+   
+
+
+}  
